7bbd995e55498d5703ddcc2b3cf8faebc2856db9
MauricioOcampo <maoocampo@hotmail.com>
2023-10-11 09:15:23 -0500

    Oct 11 de 2023 @Input

--- src/app/dbz/components/listpersonajes/listpersonajes.component.ts
+++ src/app/dbz/components/listpersonajes/listpersonajes.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character';
 
 @Component({
   selector: 'app-listpersonajes',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./listpersonajes.component.css']
 })
 export class ListpersonajesComponent {
+
+  // @Input permite recibir datos del componente padre en este caso el main
+  @Input()
+
+  public characterList: Character[] = [{
+    name: 'Mao',
+    power: 1000000000
+  }]
+
+  // Hola esto es un comentario..
 
 }
--- src/app/dbz/pages/main-page/main-page.component.ts
+++ src/app/dbz/pages/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Character } from '../../interfaces/character';
 
 @Component({
   selector: 'app-main-page',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
+
+  // NOTA: Character es un arreglo si se le omite el [] queda como un dato y muestra error
+  //       Otra cosa es que dentro de la asignacion de la ste interfaz debe ser con : NO con =
+  //       porque saca error. Estos superheroes se van a pasar a listpersonajes alla se reciben
+  //       con @Input
+  public characters: Character[] = [{
+    name: 'Matias',
+    power : 100000000
+  },
+  {
+    name : 'Camila',
+    power : 100000000
+  },{
+    name : 'Jami',
+    power : 100000000
+  },{
+    name : 'Mauricio Ocampo',
+    power : 100000000
+  }
+
+];
 
 }
